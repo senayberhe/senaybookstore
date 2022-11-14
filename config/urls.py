46187a9e9de0b06549bf92a1627798943c699ab0
senay berhe <tsionberhe@gmail.com>
2022-11-14 13:50:36 -0800

    created user authentications

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("users.url")),
     path('', include('pages.urls')),
 ]

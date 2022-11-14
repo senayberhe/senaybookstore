0c82d5bc8846335bf65328cf883ae14f9e807b62
senay berhe <tsionberhe@gmail.com>
2022-11-14 13:59:49 -0800

    added files

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("users.url")),
+    path("", include("users.url")),
     path('', include('pages.urls')),
 ]

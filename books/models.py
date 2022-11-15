f305ada641254cebb038615f3c7b78ca9fd8c7a5
senay berhe <tsionberhe@gmail.com>
2022-11-15 13:40:27 -0800

    added files

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 import uuid 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -12,11 +13,27 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
+        return reverse('book_detail', kwargs={'pk': str(self.pk)})
     
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name = 'reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review 
